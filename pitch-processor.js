2286a454838ed64f18e6d34a63d46b931c404507
Vladislav K <vv.smmservice@gmail.com>
2025-07-16 10:43:34 +0300

    Tone pitch visualization modified

--- pitch-processor.js
+++ pitch-processor.js
@@ -1,18 +1,27 @@
 class PitchProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
+    this.THRESHOLD = 0.1; // Более строгий порог
+    this.RMS_TRESHOLD = 0.002;
+    this.SEMITONE_COEFF = 1.0594630943592952645618252949463;
     this.sampleRate = 44100;
+    this.minPitch = 55; // Минимальная частота (Гц) A1
+    this.maxPitch = 1760; // Максимальная частота (Гц) A6
     this.bufferSize = 2048; // Увеличиваем размер буфера для более точного анализа
+
     this.buffer = new Float32Array(this.bufferSize);
     this.bufferIndex = 0;
-    this.THRESHOLD = 0.1; // Более строгий порог
-    this.smoothingFactor = 0.8;
-    this.lastPitch = 0;
-    this.minPitch = 50; // Минимальная частота (Гц)
-    this.maxPitch = 700; // Максимальная частота (Гц)
-    this.RMS_TRESHOLD = 0.003;
+    
+    //this.smoothingFactor = 0.8;
+    //this.lastPitch = 0;
+
+    
     this.rms = 0;
+    //this.note = 0;
+    this.noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
   }
+
+  
 
   process(inputs) {
     const input = inputs[0];
@@ -24,12 +33,23 @@ class PitchProcessor extends AudioWorkletProcessor {
       
       if (this.bufferIndex >= this.bufferSize) {
         //this.detectPitch();
-        this.autoCorrelate();
-        if ((this.lastPitch <this.minPitch) || (this.lastPitch>this.maxPitch))
-          this.lastPitch = 0;  
+        let lastPitch = this.autoCorrelate();
+        if ((lastPitch <this.minPitch) && (lastPitch !=0))
+          lastPitch = this.minPitch;
+        if (lastPitch >this.maxPitch)
+          lastPitch = this.maxPitch;  
+        let note = this.noteFromPitch(lastPitch);
+        let noteName = this.noteStrings[note%12];
+        let noteFreq = this.frequencyFromNoteNumber(note);
+        let next_freq = ((noteFreq>=this.maxPitch) || (noteFreq<=this.minPitch))? 0 : noteFreq* this.SEMITONE_COEFF;
+        let prev_freq = ((noteFreq>=this.maxPitch) || (noteFreq<=this.minPitch))? 0 : noteFreq/this.SEMITONE_COEFF;
+        //console.log(this.note);
         this.port.postMessage({
           type: 'pitch',
-          frequency: this.lastPitch,
+          prev_frequency: prev_freq,
+          frequency: lastPitch,
+          next_frequency: next_freq,
+          noteName: noteName,
           rms: this.rms
         });
         this.bufferIndex = 0;
@@ -50,9 +70,9 @@ class PitchProcessor extends AudioWorkletProcessor {
     }
     rms = Math.sqrt(rms/SIZE);
     if (rms < this.RMS_TRESHOLD) { // not enough signal
-      this.lastPitch = 0;   
+      //this.lastPitch = 0;   
       this.rms = rms;     
-      return;
+      return 0;
     } 
 
     // let r1 = 0, r2 = SIZE-1, thres = this.THRESHOLD;
@@ -84,8 +104,19 @@ class PitchProcessor extends AudioWorkletProcessor {
     let b = (x3 - x1)/2;
     if (a) T0 = T0 - b/(2*a);
 
-    this.lastPitch = sampleRate/T0;
     this.rms = rms;
+    //this.lastPitch = sampleRate/T0;
+    return sampleRate/T0;
+    
+  }
+
+  noteFromPitch( frequency ) {
+	  let noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+	  return Math.round( noteNum ) + 69;
+  }
+
+  frequencyFromNoteNumber( note ) {
+	  return 440 * Math.pow(2,(note-69)/12);
   }
 
 //   detectPitch() {
